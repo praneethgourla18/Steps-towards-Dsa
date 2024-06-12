49d6f2ef1cd5c84ae3f6dc007452792b32ea64d6
Praneeth gourla <praneethgourla@gmail.com>
2024-06-12 08:26:37 +0530

    Time: 14 ms (25.98%), Space: 48.8 MB (95.26%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -6,8 +6,8 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             int freq=1;
             if(map.containsKey(nums[i])){
-                freq=map.get(nums[i]);
-                map.put(nums[i],freq+1);
+               
+                map.put(nums[i],map.get(nums[i])+1);
             }
             else{
             map.put(nums[i],freq);
