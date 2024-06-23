f370f1e07552563be059b57a2b7c7537451abded
Praneeth gourla <praneethgourla@gmail.com>
2024-06-23 14:32:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int[] arr=new int[2];
+
+        int i=1;
+        while(i<nums.length){
+
+            int correct=nums[i]-1;
+            if(nums[correct]!=nums[i]){
+                int temp=nums[correct];
+                nums[correct]=nums[i];
+                nums[i]=temp;
+            }
+            else{
+                i++;
+            }
+        }
+        for(int j=0;j<nums.length;j++){
+            if(nums[j]!=j+1){
+                arr[0]=nums[j];
+                arr[1]=j+1;
+                // break;
+            }
+        }
+        return arr;
+    }
+}
