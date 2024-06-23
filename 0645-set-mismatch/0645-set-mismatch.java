f00384ef23afbcbb193e12b5b72366dfab5be236
Praneeth gourla <praneethgourla@gmail.com>
2024-06-23 14:33:31 +0530

    Time: 3 ms (78.67%), Space: 45 MB (91.84%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] findErrorNums(int[] nums) {
         int[] arr=new int[2];
 
-        int i=1;
+        int i=0;
         while(i<nums.length){
 
             int correct=nums[i]-1;
