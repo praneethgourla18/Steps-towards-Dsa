0c64726e40bbe9f7003451debfa2e19b8a5917a6
Praneeth gourla <praneethgourla@gmail.com>
2024-07-03 13:11:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -1,22 +1,17 @@
 class Solution {
     public boolean halvesAreAlike(String s) {
-        String a="";
-        String b="";
-        int count=0;
-        int sum=0;
-        for(int i=0;i<s.length()/2;i++){
-            a+=s.charAt(i);
-            if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||     s.charAt(i)=='u'||s.charAt(i)=='A'||s.charAt(i)=='E'||s.charAt(i)=='I'||s.charAt(i)=='O'||s.charAt(i)=='U'){
-                count++;
+        int v1=0;
+        int v2=0;
+        for(int i=0;i<(s.length())/2;i++){
+            if(s.charAt(i)=='a' ||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u'){
+                v1++;
             }
         }
-        for(int i=s.length()/2;i<s.length();i++){
-           b+=s.charAt(i);
-            if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||     s.charAt(i)=='u'||s.charAt(i)=='A'||s.charAt(i)=='E'||s.charAt(i)=='I'||s.charAt(i)=='O'||s.charAt(i)=='U'){
-               sum++;
+         for(int i=(s.length())/2;i<s.length();i++){
+            if(s.charAt(i)=='a' ||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u'){
+                v2++;
             }
         }
-        return count==sum;
-        
+        return v1==v2;
     }
 }
