dac0bb80118693befd028308ddfbfb390cb7a721
Praneeth gourla <praneethgourla@gmail.com>
2024-07-04 16:38:39 +0530

    Time: 4 ms (96.22%), Space: 44.2 MB (67.74%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -1,20 +1,43 @@
 class Solution {
     public boolean judgeCircle(String moves) {
 
-        HashMap<Character,Integer> map=new HashMap<>();
-        for(int i=0;i<moves.length();i++){
+        //brute force
+
+    //     HashMap<Character,Integer> map=new HashMap<>();
+    //     for(int i=0;i<moves.length();i++){
            
-        if(!map.containsKey(moves.charAt(i))){
-          map.put(moves.charAt(i),0);
-        }
-         else{
-            map.put(moves.charAt(i),map.get(moves.charAt(i))+1);
-         }  
+    //     if(!map.containsKey(moves.charAt(i))){
+    //       map.put(moves.charAt(i),0);
+    //     }
+    //      else{
+    //         map.put(moves.charAt(i),map.get(moves.charAt(i))+1);
+    //      }  
            
+    //     }
+    //    int horizontal = map.getOrDefault('R', 0) - map.getOrDefault('L', 0);
+    //     int vertical = map.getOrDefault('U', 0) - map.getOrDefault('D', 0);
+        
+    //     return horizontal == 0 && vertical == 0;
+
+
+    
+        int horizontal = 0;
+        int vertical = 0;
+        
+        for (char move : moves.toCharArray()) {
+            if (move == 'L') {
+                horizontal--;
+            } else if (move == 'R') {
+                horizontal++;
+            } else if (move == 'U') {
+                vertical++;
+            } else if (move == 'D') {
+                vertical--;
+            }
         }
-       int horizontal = map.getOrDefault('R', 0) - map.getOrDefault('L', 0);
-        int vertical = map.getOrDefault('U', 0) - map.getOrDefault('D', 0);
         
         return horizontal == 0 && vertical == 0;
+
+
     }
 }
