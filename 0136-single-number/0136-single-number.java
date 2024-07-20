b16b33960c9246db7248ae3511c619f87a8e45c4
Praneeth gourla <praneethgourla@gmail.com>
2024-07-20 19:27:50 +0530

    Time: 1 ms (99.89%), Space: 46.1 MB (38.16%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,25 +1,24 @@
 class Solution {
     public int singleNumber(int[] arr) {
-        // int result=0;
-        // for(int i=0;i<arr.length;i++){
-        //     result=result ^ arr[i];
-        // }
-        // return result;
-
-        HashMap<Integer,Integer> map=new HashMap<>();
+        int result=0;
         for(int i=0;i<arr.length;i++){
-            if(map.containsKey(arr[i])){
-                map.put(arr[i],map.get(arr[i])+1);
-            }
-            else{
-            map.put(arr[i],1);
-            }
+        result=result ^ arr[i];
         }
-        for(int key : map.keySet()){
-            if(map.get(key)==1){
-                return key;
-            }
-        }
-        return -1;
+        return result;
+
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // for (int i = 0; i < arr.length; i++) {
+        //     if (map.containsKey(arr[i])) {
+        //         map.put(arr[i], map.get(arr[i]) + 1);
+        //     } else {
+        //         map.put(arr[i], 1);
+        //     }
+        // }
+        // for (int key : map.keySet()) {
+        //     if (map.get(key) == 1) {
+        //         return key;
+        //     }
+        // }
+        // return -1;
     }
 }
