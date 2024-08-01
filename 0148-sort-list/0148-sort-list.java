76d2ddcf22d478cfc1edefe9ef7d79afad41d295
Praneeth gourla <praneethgourla@gmail.com>
2024-08-01 16:27:54 +0530

    Time: 9 ms (92.38%), Space: 57.3 MB (16.32%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -8,25 +8,86 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode sortList(ListNode head) {
+//         ListNode old=head;
+//         int size=0;
+//         while(old!=null){
+//             size++;
+//             old=old.next;
+//         }
+//         for(int i=0;i<size;i++){
+//             ListNode temp=head;
+//             while(temp.next!=null){
+//                 if(temp.val>temp.next.val){
+//                     int s=temp.val;
+//                     temp.val=temp.next.val;
+//                     temp.next.val=s;
+//                 }
+//                 temp=temp.next;
+//             }
+//         }
+//         return head;
+//     }
+// }
+
+
+
+
 class Solution {
     public ListNode sortList(ListNode head) {
-        ListNode old=head;
-        int size=0;
-        while(old!=null){
-            size++;
-            old=old.next;
+        if (head == null || head.next == null) {
+            return head;
         }
-        for(int i=0;i<size;i++){
-            ListNode temp=head;
-            while(temp.next!=null){
-                if(temp.val>temp.next.val){
-                    int s=temp.val;
-                    temp.val=temp.next.val;
-                    temp.next.val=s;
-                }
-                temp=temp.next;
+
+        ListNode middle = getMiddle(head);
+        ListNode nextOfMiddle = middle.next;
+        middle.next = null;
+
+        ListNode left = sortList(head);
+        ListNode right = sortList(nextOfMiddle);
+
+        return merge(left, right);
+    }
+
+    private ListNode getMiddle(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    private ListNode merge(ListNode left, ListNode right) {
+        ListNode result = new ListNode();
+        ListNode current = result;
+
+        while (left != null && right != null) {
+            if (left.val <= right.val) {
+                current.next = left;
+                left = left.next;
+            } else {
+                current.next = right;
+                right = right.next;
             }
+            current = current.next;
         }
-        return head;
+
+        if (left != null) {
+            current.next = left;
+        } else if (right != null) {
+            current.next = right;
+        }
+
+        return result.next;
     }
 }
+
+
+
+
+
