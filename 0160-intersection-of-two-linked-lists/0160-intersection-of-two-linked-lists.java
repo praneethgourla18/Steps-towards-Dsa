36915c547cf1e4ff00abe34f43ab53c176e36424
Praneeth gourla <praneethgourla@gmail.com>
2024-08-02 19:35:23 +0530

    Time: 1 ms (99.91%), Space: 48.2 MB (72.05%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -27,5 +27,51 @@ public class Solution {
         //     temp=temp.next;
         // }
         // return null;
+
+          
+        if (headA == null || headB == null) return null;
+        
+        ListNode temp1 = headA;
+        ListNode temp2 = headB;
+        int size1 = 0;
+        int size2 = 0;
+
+      
+        while (temp1 != null) {
+            size1++;
+            temp1 = temp1.next;
+        }
+        while (temp2 != null) {
+            size2++;
+            temp2 = temp2.next;
+        }
+
+      
+        temp1 = headA;
+        temp2 = headB;
+
+      
+        if (size1 > size2) {
+            int n = size1 - size2;
+            for (int i = 0; i < n; i++) {
+                temp1 = temp1.next;
+            }
+        } else {
+            int n = size2 - size1;
+            for (int i = 0; i < n; i++) {
+                temp2 = temp2.next;
+            }
+        }
+
+      
+        while (temp1 != null && temp2 != null) {
+            if (temp1 == temp2) {
+                return temp1;
+            }
+            temp1 = temp1.next;
+            temp2 = temp2.next;
+        }
+
+        return null;  
     }
 }
