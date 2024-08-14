8d141e6a5b5c1be3c406b49cc5f28bcaf160e132
Praneeth gourla <praneethgourla@gmail.com>
2024-08-14 14:48:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,28 +1,27 @@
 class Solution {
     public boolean isValid(String s) {
-          Stack<Character> stack = new Stack<>();
+        if(s.length()%2!=0){
+            return false;
+        }
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='['){
+                st.push(s.charAt(i));
+            }
+            else{
+                char top=st.peek();
+                char c=s.charAt(i);
+                if((c == ')' && top == '(') ||
+                   (c == '}' && top == '{') ||
+                   (c == ']' && top == '[')) {
+                    st.pop(); // Pop the matching opening bracket
+                }
 
-    for (char ch : s.toCharArray()) {
-      if (ch == '(' || ch == '{' || ch == '[') {
-        stack.push(ch);
-      } else {
-        if (ch == ')') {
-          if (stack.isEmpty() || stack.pop() != '(') {
-            return false;
-          }
+                else{
+                    return false;
+                }
+            }
         }
-        if (ch == '}') {
-          if (stack.isEmpty() || stack.pop() != '{') {
-            return false;
-          }
-        }
-        if (ch == ']') {
-          if (stack.isEmpty() || stack.pop() != '[') {
-            return false;
-          }
-        }
-      }
-    }
-    return stack.isEmpty();
+        return true;
     }
 }
