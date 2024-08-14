434e31672a086c251cd37617d83bf4d4905324f0
Praneeth gourla <praneethgourla@gmail.com>
2024-08-14 13:15:11 +0530

    Time: 4 ms (43.61%), Space: 44.2 MB (22.94%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] nextGreaterElement(int[] arr, int[] arr2) {
+          Stack<Integer> s1 = new Stack<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] ans = new int[arr.length];
+
+        // Traverse arr2 to find the next greater elements
+        for (int i = arr2.length - 1; i >= 0; i--) {
+            while (!s1.isEmpty() && s1.peek() <= arr2[i]) {
+                s1.pop();
+            }
+            map.put(arr2[i], s1.isEmpty() ? -1 : s1.peek());
+            s1.push(arr2[i]);
+        }
+
+        // Populate the ans array based on the elements in arr
+        for (int i = 0; i < arr.length; i++) {
+            ans[i] = map.getOrDefault(arr[i], -1);
+        }
+
+        return ans;
+    }
+}
