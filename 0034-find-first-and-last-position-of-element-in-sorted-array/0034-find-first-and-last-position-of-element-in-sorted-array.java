bbf4bd790738c3bc97d1139b50d5186b0b892179
Praneeth gourla <praneethgourla@gmail.com>
2024-11-06 15:32:05 +0530

    Time: 0 ms (100%), Space: 46.1 MB (9.26%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,35 +1,60 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        boolean sir=true;
-        int left=firstocc( nums,target,true);
-        int right=firstocc(nums,target,false);
-        
-        return new int[]{left,right};
+        int[] arr=new int[2];
+        arr[0]=binarySearch(nums,target,true);
+        arr[1]=binarySearch(nums,target,false);
+
+        return arr;
     }
-        static int firstocc(int[] nums,int target,boolean sir){
-        int res=-1;
+    static int binarySearch(int[] nums,int target,boolean first){
         int start=0;
         int end=nums.length-1;
+        int ans=-1;
         while(start<=end){
-            
-            int mid= start +(end - start)/2;
-            if(target==nums[mid]){   
-                res=mid;
-               if(sir)
-                end=mid-1;
-               else
-                   start=mid+1;
-            }
-            else if(target>nums[mid]){
-                    start=mid+1;                
-            }
-            else{
-                 end=mid-1;
-            }
-        }
-         return  res;
-    }
-      
-     
-    }
+            int mid=start+(end-start)/2;
 
+            // if(first){
+            //     if(nums[mid]==target){
+            //         ans=mid;
+            //         end=mid-1;
+            //     }
+            //     else if(nums[mid]>target){
+            //         end=mid-1;
+            //     }
+            //     else{
+            //         start=mid+1;
+            //     }
+            // }
+            // else{
+            //      if(nums[mid]==target){
+            //         ans=mid;
+            //         start=mid+1;
+            //     }
+            //     else if(nums[mid]>target){
+            //         end=mid-1;
+            //     }
+            //     else{
+            //         start=mid+1;
+            //     }
+            // }
+            if(nums[mid]==target){
+                   if(first){
+                      ans=mid;
+                      end=mid-1;
+                   }
+                   else{
+                      ans=mid;
+                      start=mid+1;
+                   }
+                   
+                }
+                else if(nums[mid]>target){
+                    end=mid-1;
+                }
+                else{
+                    start=mid+1;
+                }
+        }
+        return ans;
+    }
+}
